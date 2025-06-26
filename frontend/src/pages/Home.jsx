f31b2026e9a1a9d9e96fb5578876654ff83ff136
MadCoder7 <shankarpubg4@gmail.com>
2025-06-26 15:25:04 +0530

    Initial Commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import Listings from "../components/Listings";
 import Explore from "../components/Explore";
-import StayUpdated from "../components/Stayupdated";
+import StayUpdated from "../components/StayUpdated";
 import Footer from "../components/Footer";
 
 const Home = () => {
